1bbc0c0ec58c5e83e54bdf667ac337d6a04e3fa4
srivastava-harshal <harshalsrivastava07@gmail.com>
2022-10-20 02:49:34 +0530

    reverted changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import App from './App';
-import About from './components/About';
-import MySkills from './components/MySkills';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/my-skills" element={<MySkills />} />
-      <Route path="/work" element={<Work />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
