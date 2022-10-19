ac4757874886fd509eedda0b123621aa27a81a3b
srivastava-harshal <harshalsrivastava07@gmail.com>
2022-10-20 02:42:15 +0530

    added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import MySkills from './components/MySkills';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/my-skills" element={<MySkills />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   </BrowserRouter>
 );
